d47d081f703154ff08e3422b554ddc7b35e0a4bd
11421881 <11421881@ESTB80706.unibh.edu>
2015-09-08 22:30:39 -0300

    ok

---
+++ pessoas/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
@@ -0,0 +1,40 @@
+package br.unibh.pessoas.persistencia;
+
+import java.util.List;
+
+public class PessoaFisicaDAO<PessoaFisica> implements DAO<PessoaFisica, Long>{
+
+	@Override
+	public PessoaFisica find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(PessoaFisica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(PessoaFisica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(PessoaFisica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PessoaFisica> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+}
