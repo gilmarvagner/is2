2e1c509c17e533cd337f1333598cbdf4ed7a23e2
11421881 <11421881@ESTB80706.unibh.edu>
2015-09-14 20:49:08 -0300

    ok

--- pessoas/src/br/unibh/pessoas/Main.java
+++ pessoas/src/br/unibh/pessoas/Main.java
@@ -1,17 +1,15 @@
 package br.unibh.pessoas;
 
-import java.util.Date;
-
-import br.unibh.pessoas.entidades.Pessoa;
 import br.unibh.pessoas.entidades.PessoaFisica;
+import br.unibh.pessoas.persistencia.PessoaFisicaDAO;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		PessoaFisica pf1 = new PessoaFisica(1l, "Joao", "Rua A", "3333-33333", "111222333444", "joao@gmail.com", new Date(),"M");
-		System.out.println(pf1);
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		dao.findAll();
 	}
 
 }
--- pessoas/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
+++ pessoas/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
@@ -1,5 +1,6 @@
 package br.unibh.pessoas.persistencia;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 public class PessoaFisicaDAO<PessoaFisica> implements DAO<PessoaFisica, Long>{
@@ -31,6 +32,17 @@ public class PessoaFisicaDAO<PessoaFisica> implements DAO<PessoaFisica, Long>{
 	@Override
 	public List<PessoaFisica> findAll() {
 		// TODO Auto-generated method stub
+		try {
+			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_pessoa_fisica").executeQuery();
+			while (res.next()){
+				System.out.println(res.getString("nome"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			
+		}
+		
 		return null;
 	}
 
